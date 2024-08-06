b9815ebe67d24ac6843d74b5773521416c05e2a6
kajtekajtek <kajtek0123@gmail.com>
2024-08-06 12:46:47 +0200

    added draw_rectangle() function

--- include/raycaster.h
+++ include/raycaster.h
@@ -3,9 +3,15 @@
 
 // colors are stored in four byte integers
 // functions pack_color and unpack_color allow working with color channels
-uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a);
+uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, 
+                                                    const uint8_t a);
 
-void unpack_color(uint32_t *color, uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *a);
+void unpack_color(uint32_t *color, uint8_t *r, uint8_t *g, uint8_t *b, 
+                                                        uint8_t *a);
 
 // create .ppm image from a one dimensional pixel array
 void create_ppm_img(char *filename, uint32_t *img, size_t w, size_t h);
+
+void draw_rectangle(uint32_t *img, size_t img_w, size_t img_h, size_t x, 
+                        size_t y, size_t w, size_t h, uint32_t color);
+
--- src/raycaster.c
+++ src/raycaster.c
@@ -1,13 +1,15 @@
-#include "raycaster.h"
+#include "../include/raycaster.h"
 
 // colors are stored in four byte integers
 // functions pack_color and unpack_color allow working with color channels
-uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a)
+uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, 
+                                                    const uint8_t a)
 {
     return (a<<24) + (b<<16) + (g<<8) + r;
 }
 
-void unpack_color(uint32_t *color, uint8_t *r, uint8_t *g, uint8_t *b, uint8_t *a)
+void unpack_color(uint32_t *color, uint8_t *r, uint8_t *g, uint8_t *b,
+                                                        uint8_t *a)
 {
     *r = (*color >> 0) & 255;
     *g = (*color >> 8) & 255;
@@ -24,7 +26,7 @@ void create_ppm_img(char *filename, uint32_t *img, size_t w, size_t h)
     file_ptr = fopen(filename, "wb");
 
     if (file_ptr == NULL) {
-        printf("Error: the image was not created. Exiting now.");
+        printf("Error: the image was not created. Exiting now...\n");
         exit(1);
     }
 
@@ -55,4 +57,22 @@ void create_ppm_img(char *filename, uint32_t *img, size_t w, size_t h)
     fclose(file_ptr);
 
     printf("Image saved to %s.\n", filename);
+}
+
+void draw_rectangle(uint32_t *img, size_t img_w, size_t img_h, size_t x, 
+                        size_t y, size_t w, size_t h, uint32_t color)
+{
+    for (size_t i = 0; i < w; i++) {
+        for (size_t j = 0; j < h; j++) {
+            size_t cx = x + i;
+            size_t cy = y + j;
+
+            if (cx >= img_w && cy >= img_h) {
+                printf("Error: draw_rectangle(). Exiting now...\n");
+                exit(1);
+            }
+
+            img[cx + cy * img_w] = color;
+        }
+    }
 }
